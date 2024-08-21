b559ede796caef03719b5d3cff3c282d666b2ae9
LeyzerGurewitz <leyzer.3770@gmail.com>
2024-08-21 15:52:50 +0300

    Create

---
+++ Rest/AgentRestApi/AgentRestApi/Models/AgentModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgentRestApi.Models
+{
+    public class AgentModel
+    {
+        public enum Status
+        {
+            offDuty,
+            Activete
+            
+        }
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string NickName { get; set; }
+
+        [Required, StringLength (100)]
+        public string PhotoUrl { get; set; }
+
+        public int LocationX { get; set; } = -1;
+
+        public int LocationY { get; set; } = -1;
+
+        public Status Agentstatus { get; set; }
+
+        public List<MissionModel> Missions { get; set; } = [];   
+
+
+    }
+}
---
+++ Rest/AgentRestApi/AgentRestApi/Models/MissionModel.cs
@@ -0,0 +1,27 @@
+﻿namespace AgentRestApi.Models
+{
+    public class MissionModel
+    {
+        public enum Status
+        {
+            taskProposal,
+            MitzvahForTheTask,
+            TheMissionIsOver
+
+        }
+        public int Id { get; set; }
+
+        public int TargetID { get; set; }
+
+        public int AgentID { get; set; }
+        public TargetsModel Targets { get; set; }
+        public AgentModel Agent { get; set; }
+
+        public double TimeLeft { get; set; }
+
+        public double ExecuteTime {  get; set; }
+
+        public Status MissionStatus { get; set; }
+
+    }
+}
